c6c031c9c257a2f224281adcfa95438ebfb19ad9
Your Name <Your Github Email Address>
2021-09-28 14:46:02 +0000

    completed some questions

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 //1
 function countToFifty(){
-  
+  for (let i = 1; i <= 50; i++){
+      console.log(i)
+  }  
 }
+countToFifty()
 
 //2
-function countFromOne(){
-  
+function countFromOne(number){
+  for (let i = 1; i <= number; i++){
+      console.log(i)
+  }
 }
-
+countFromOne(5)
 //3
 function threeAndFiveInFifty(){
   
@@ -18,4 +23,11 @@ function threeAndFive(){
   
 }
 
+function triple(numbers){
+ let result = numbers.map(function(num){
+    return num * 3;
+});
+  return result;
+}
 
+console.log( triple([1, 2, 3]) )
